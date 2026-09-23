c16970864bec0e02164f65dcc7f120acb55dbc98
Daniel McGuire <danielmcguire23@icloud.com>
2026-09-23 12:28:11 -0500

    Check for compiler at config time

--- cmake/Compiler/Compiler.cmake
+++ cmake/Compiler/Compiler.cmake
@@ -1,4 +1,5 @@
 include("${CMAKE_CURRENT_LIST_DIR}/CompilerFlags.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/InlineAssembly.cmake")
 
 if(WIN32 AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     include(${CMAKE_CURRENT_LIST_DIR}/ClangFix.cmake)
